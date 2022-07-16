83e17469859549f0ecd9290dcf3d8034be4c1b48
Doğukan Çalışkan <dogukan.engt@gmail.com>
2022-07-16 20:12:57 +0300

    transition and shadow on countries on hover

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -4,7 +4,7 @@ import React from 'react';
 export default function Countries(props){
 
     return (
-                <div className="rounded-md w-5/6 lg:w-full h-[370px] bg-white dark:bg-dark-blue drop-shadow-md mb-11" onClick={props.onClick}>
+                <div className="rounded-md w-5/6 lg:w-full h-[370px] bg-white dark:bg-dark-blue drop-shadow-md hover:shadow-[1px_1px_5px_2px] cursor-pointer transition duration-500 hover:scale-105 mb-11" onClick={props.onClick}>
                     <img className="w-full h-1/2 rounded-md" src={props.flag} alt="country flag"/>
                     <div className="h-1/2 pt-7 pl-7 dark:text-white">
                         <h2 className="font-extrabold mb-3">{props.country}</h2>
